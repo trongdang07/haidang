a3d8f6a1462387a96e46aa8950962543f288034b
Lê Trọng Hải Đăng <135571277+trongdang07@users.noreply.github.com>
2024-11-05 21:19:40 +0700

    Add files via upload

---
+++ chuot.js
@@ -0,0 +1,5 @@
+// Đổi hình ảnh con trỏ chuột
+document.body.style.cursor = 'url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi2UiwkC42_TYds4b_D8KSno9kj9OIihVmwdYfTanM12xQC9dlWLaDpY8UM74mscXm2OJwMQ30HC-OEvpvSYNRAFttVLIiIUmiFCDx8oRMYkdOTc6nxAp0nbWyzz9qGTXCuCoHizwiIiGcZPHTOhigPrvlrhA0d7yHlq3lRr0Bczp5QlIKmu5mSo49_/s70/curmiku6.png), auto';
+
+// Đổi hình ảnh bàn tay chuột
+document.body.style.cursor = 'url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEinyFDof7iQ3YFkue7SgoEgD0p5qnPA72KGGkQVpF_bYb3wwHAbhCn_yCXvWgCt_C7RKSNJDN1GacZYndk9tXXP0oC841r7p1UQv1jdNcGo5m1huthXutLaX59XJXXgoeryxQ0U8U0oBYwNoph23irkUJqXdMJOx-HHyXAUVr85pAXjgOR9uYnZMgzZ/s1600/curmiku9.png), pointer';
