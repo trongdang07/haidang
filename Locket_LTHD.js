ddbcdeeffccc8c4815c6a8be6fc96bf89e216ce2
Lê Trọng Hải Đăng <135571277+trongdang07@users.noreply.github.com>
2025-01-05 19:35:20 +0700

    Create Locket_LTHD.js

---
+++ Locket_LTHD.js
@@ -0,0 +1,54 @@
+// ========= ID ========= //
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
+};
+
+// ========= Phần cố định ========= //
+// ========= @DuongVanSy ========= //
+
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+var obj = JSON.parse($response.body);
+
+// Thông báo người dùng
+obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+
+// Định nghĩa dữ liệu giả lập
+var dvsteam = {
+  is_sandbox: false,
+  ownership_type: "PURCHASED",
+  billing_issues_detected_at: null,
+  period_type: "normal",
+  expires_date: "2099-12-18T01:04:17Z",
+  grace_period_expires_date: null,
+  unsubscribe_detected_at: null,
+  original_purchase_date: "2007-09-30T09:45:00Z",
+  purchase_date: "2007-09-30T09:45:00Z",
+  store: "app_store"
+};
+
+var dvsteam_subscription = {
+  grace_period_expires_date: null,
+  purchase_date: "2007-09-30T09:45:00Z",
+  product_identifier: "com.dvsteam.premium.yearly",
+  expires_date: "2099-12-18T01:04:17Z"
+};
+
+// Kiểm tra User-Agent và ánh xạ quyền
+const match = Object.keys(mapping).find(e => ua.includes(e));
+if (match) {
+  let [entitlement, product] = mapping[match];
+  if (product) {
+    dvsteam_subscription.product_identifier = product;
+    obj.subscriber.subscriptions[product] = dvsteam;
+  } else {
+    obj.subscriber.subscriptions["com.dvsteam.premium.yearly"] = dvsteam;
+  }
+  obj.subscriber.entitlements[entitlement] = dvsteam_subscription;
+} else {
+  obj.subscriber.subscriptions["com.dvsteam.premium.yearly"] = dvsteam;
+  obj.subscriber.entitlements.pro = dvsteam_subscription;
+}
+
+// Trả về kết quả
+$done({ body: JSON.stringify(obj) });
